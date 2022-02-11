95a03745e1c754926cc2770b40411dd0b916b1c8
Bidit Mangal <39191019+Biditmangal@users.noreply.github.com>
2022-02-11 12:30:28 +0530

    Update bot_utils.py

--- bot/helper/ext_utils/bot_utils.py
+++ bot/helper/ext_utils/bot_utils.py
@@ -128,8 +128,8 @@ def get_readable_message():
                 globals()['PAGE_NO'] -= 1
             START = COUNT
         for index, download in enumerate(list(download_dict.values())[START:], start=1):
-            msg += f"✍️<b>Name:</b> <code>{escape(str(download.name()))}</code>"
-            msg += f"\n⏳<b>Status:</b> <i>{download.status()}</i>"
+            msg += f"✍️ <b>Name:</b> <code>{escape(str(download.name()))}</code>"
+            msg += f"\n⏳ <b>Status:</b> <i>{download.status()}</i>"
             if download.status() not in [
                 MirrorStatus.STATUS_ARCHIVING,
                 MirrorStatus.STATUS_EXTRACTING,
@@ -142,8 +142,8 @@ def get_readable_message():
                 elif download.status() == MirrorStatus.STATUS_UPLOADING:
                     msg += f"\n<b>Uploaded:</b> {get_readable_file_size(download.processed_bytes())} of {download.size()}"
                 else:
-                    msg += f"\n📥<b>Downloaded:</b> {get_readable_file_size(download.processed_bytes())} of {download.size()}"
-                msg += f"\n<b>Speed:</b> {download.speed()} | <b>⏰ETA:</b> {download.eta()}"
+                    msg += f"\n📥 <b>Downloaded:</b> {get_readable_file_size(download.processed_bytes())} of {download.size()}"
+                msg += f"\n<b>Speed:</b> {download.speed()} | <b>⏰ ETA:</b> {download.eta()}"
                 try:
                     msg += f"\n☣️<b>Seeders:</b> {download.aria_download().num_seeders}" \
                            f" | <b>Peers:</b> {download.aria_download().connections}"
@@ -151,7 +151,7 @@ def get_readable_message():
                     pass
                 try:
                     msg += f"\n☣️<b>Seeders:</b> {download.torrent_info().num_seeds}" \
-                           f" | 🧲<b>Leechers:</b> {download.torrent_info().num_leechs}"
+                           f" | <b>Leechers:</b> {download.torrent_info().num_leechs}"
                 except:
                     pass
                 msg += f"\n❌<b>Stop: </b><code>/{BotCommands.CancelMirror} {download.gid()}</code>"
@@ -170,7 +170,7 @@ def get_readable_message():
         total, used, free, _ = disk_usage('.')
         free = get_readable_file_size(free)
         currentTime = get_readable_time(time() - botStartTime)
-        bmsg = f"⚙️<b>CPU:</b> {cpu_percent()}% | 🔋<b>FREE:</b> {free}"
+        bmsg = f"⚙ <b>CPU:</b> {cpu_percent()}% | <b>FREE:</b> {free}"
         for download in list(download_dict.values()):
             speedy = download.speed()
             if download.status() == MirrorStatus.STATUS_DOWNLOADING:
@@ -185,8 +185,8 @@ def get_readable_message():
                     uldl_bytes += float(speedy.split('M')[0]) * 1048576
         dlspeed = get_readable_file_size(dlspeed_bytes)
         ulspeed = get_readable_file_size(uldl_bytes)
-        bmsg += f"\n🗃️<b>RAM:</b> {virtual_memory().percent}% | 🔧<b>UPTIME:</b> {currentTime}"
-        bmsg += f"\n🔻<b>DL:</b> {dlspeed}/s | 🔺<b>UL:</b> {ulspeed}/s"
+        bmsg += f"\n<b>RAM:</b> {virtual_memory().percent}% | <b>UPTIME:</b> {currentTime}"
+        bmsg += f"\n<b>DL:</b> {dlspeed}/s | <b>UL:</b> {ulspeed}/s"
         if STATUS_LIMIT is not None and tasks > STATUS_LIMIT:
             msg += f"<b>Page:</b> {PAGE_NO}/{pages} | <b>Tasks:</b> {tasks}\n"
             buttons = ButtonMaker()
